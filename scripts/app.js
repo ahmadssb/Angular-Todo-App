b4bba362563fa5a02ae111d96a9b9fd2990d23cd
Ahmed Babadr <ahmad@ahmadssb.com>
2015-10-21 13:28:50 +0300

    Feat: create todoListController and display list on html

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,25 @@
+todos = [
+	{
+		"text": "task 1",
+		"done": false,
+	},
+	{
+		"text": "task 2",
+		"done": false,
+	}
+];
 angular.module('todoApp', [])
-	.controller('MyAppController', function () {
-
+	.controller('TodoListController', function () {
+		var self = this;
+		self.todoList = todos;
+		self.addTodo = function () {
+			self.todoList.push({"text": self.text, "done": false});
+			console.log("addTodo Clicked - new data:" + self.text);
+			self.text = '';
+			console.log(self.todoList);
+		};
+		self.remaining = function () {
+			
+		};
+		
 	});
